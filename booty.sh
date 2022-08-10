a3b2074287f5f0f6aad860f68287bc607bbcb7ef
Sultranos <110790178+Sultranos@users.noreply.github.com>
2022-08-10 22:33:47 +0200

    Add files via upload

--- booty.sh
+++ booty.sh
@@ -4,77 +4,162 @@ echo "Bonjour Nouvel Utilisateur,"
 echo "a qui ai-je l'honneur de parler?"
     read varname
     echo "Bienvenue sur le systeme $varname"
-    echo "Je vais preparer le systeme pour vous."
+    echo "Desirez vous ajouter un utilisateur pi avec UID1000? (y/n)"
+        read pi
+        if [[ $pi == "y" ]]; then
+            echo "L'utilisateur pi seras ajouté lors de l'installation"
+        else 
+            echo "Vous serez le seul utilisateur ajouter lors de l'installation"
+        fi
 
-
+    echo "Je vais prendre en charge la preparation du systeme pour vous."
+echo ""
+echo ""
 echo "###################################################################"
 echo "                 ------- Preparation pour Nala ------"
 echo "###################################################################"
 echo ""
-sleep 3
-
+sleep 2
     echo "Instalation des repertoires"    
     echo "deb https://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
     wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
-
+echo ""
     echo "Source Repo"
     echo "deb-src https://deb.volian.org/volian/ scar main" | sudo tee -a /etc/apt/sources.list.d/volian-archive-scar-unstable.list
-
+echo ""
 sleep 2
 echo "###################################################################"
 echo "                ------- Installation de Nala ------"
 echo "###################################################################"
 echo ""
 sleep 2
-
     sudo apt update
     echo "updated, installing Nala"
     sudo apt install nala -y || sudo apt install nala-legacy -y 
     echo "Nala is Installed"
-
+echo ""
 sleep 2
 echo "###################################################################"
 echo "                 -------Mise a jour Nala------"
 echo "###################################################################"
 echo ""
 sleep 2
-
+echo ""
     sudo nala update && sudo nala upgrade -y
-
+echo ""
 sleep 2
 echo "###################################################################"
 echo "              ----- Mise a niveau Logiciels -----"
 echo "###################################################################"
 sleep 2
-
+echo ""
     sudo nala install htop -y 
     sudo nala install neofetch -y
     sudo nala install docker -y
-    sudo nala install tmux -y
-
+    sudo nala install mosquitto mosquitto-clients -y
+       echo "Creer MDP pour Mosquitto"
+       read varpasswd
+       echo "$varname:$varpasswd" > pwfile
+       mosquitto_passwd -U pwfile
+       cat pwfile
+       sudo mv pwfile /etc/mosquitto/
+       echo "copier et coller les lignes suivante dans le dos au dessu de "include_dir" "
+       sleep 1
+       echo ""
+            echo "allow_anonymous false"
+            echo "password_file /etc/mosquitto/pwfile"
+            echo "contiue ? (Y/n)"
+                read answer
+                if [[ $answer == "n" ]]: then
+                    sleep 5 echo "contiue ? (Y/n)"
+                read answer
+                if [[ $answer == "n" ]]: then
+                    sleep 5 echo "contiue ? (Y/n)"
+                read answer
+                if [[ $answer == "n" ]]: then
+                    sleep 5 
+                fi 
+        sudo nano /etc/mosquitto/mosquitto.conf
+        sudo /etc/init.d/mosquitto restart
+    wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+    lsb_release -a
+    echo "deb https://repos.influxdata.com/debian buster stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+    sudo nala update
+    sudo nala install influxdb -y
+    sudo systemctl unmask influxdb
+    sudo systemctl enable influxdb
+    sudo systemctl start influxdb
+    influx
+    CREATE USER admin WITH PASSWORD 'adminpassword' WITH ALL PRIVILEGES
+    exit
+    echo "faire CTRL+W et chercher HTTP, faire les reglage suivants: "
+        sleep 1
+        echo ""
+            echo "auth-enabled = true"
+            echo "pprof-enabled = true"
+            echo "pprof-auth-enabled = true"
+            echo "ping-auth-enabled = true"
+            echo "contiue ? (Y/n)"
+                read answer
+                if [[ $answer == "n" ]]: then
+                    sleep 5 echo "contiue ? (Y/n)"
+                read answer
+                if [[ $answer == "n" ]]: then
+                    sleep 5 echo "contiue ? (Y/n)"
+                read answer
+                if [[ $answer == "n" ]]: then
+                    sleep 5 
+                fi 
+    sudo nano /etc/influxdb/influxdb.conf
+    sudo systemctl restart influxdb
+    influx -username $varname -password <$varpasswd>
+    CREATE DATABASE sensors
+    exit
+    sudo nala install build-essential git
+    bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+    npm install node-red-contrib-influxdb
+    sudo systemctl enable nodered.service
+    sudo systemctl start nodered.service
+    wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+    echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+    sudo nala update
+    sudo nala install grafana -y
+    sudo systemctl enable grafana-server
+    sudo systemctl start grafana-server
+    mosquitto_sub -u mqtt_username -P mqtt_password -v -t "#" 
+    while sleep 5 then exit
+echo ""
 sleep 2
 echo "###################################################################"
 echo "                 -------System mis a jour------"
 echo "###################################################################"
 echo ""
+sleep 1
 echo " programmes installés:
         - Neofetch
         - HTOP
         - Docker
-        - Tmux"
-
-
+        - Mosquitto-Broker
+        - NodeRed
+        - Grafana
+        -"
+     
+echo ""
+sleep 2
 echo "##########################################################################"
 echo "                   ----- Creation des Utilisateurs -----"
 echo "##########################################################################"
 sleep 2 
 
-useradd -u 1000 -m -k -N -s /bin/bash -G sudo pi 
-addgroup --force-badname Many-Faces-God
-usermod -aG Many-Faces-God pi 
-
+if [[ $pi == "y" ]]; then
+        useradd -u 1000 -m -k -N -s /bin/bash -G sudo pi || sudo usermod -aG sudo pi
+        addgroup --force-badname Many-Faces-God
+        usermod -aG Many-Faces-God pi 
     echo "Utilisateur pi créé"
     echo "Utilisateur pi a été ajouté au groupes d'administration"
+else 
+        addgroup --force-badname Many-Faces-God
+fi
 
 adduser --force-badname $varname
 usermod -aG sudo $varname && usermod -aG Many-Faces-God $varname
@@ -82,24 +167,32 @@ usermod -aG sudo $varname && usermod -aG Many-Faces-God $varname
  echo "Utilisateur $varname créé"
  echo "Utilisateur $varname a été ajouté au groupes d'administration"
 
-echo "##########################################################################"
-echo "                   ----- Dernieres Modifications -----"
-echo "##########################################################################"
-sleep 2 
-     echo "supression de Booty.sh des fichier executables"
-     chmod -x /booty.sh
-     mv  booty.sh .booty_shaked.sh  
-     echo ""
 
 echo "###################################################################"
-tmux
-\^b%
-neofetch
-\^b%
-htop
 
+gnome-terminal htop &
+neofetch
 sleep 2
  echo "$varname, le systeme est pret à etre utilisé"
  echo "Enjoy =) "
 
  sudo su - $varname
+
+
+
+
+
+
+
+
+
+
+echo "##########################################################################"
+echo "                   ----- Dernieres Modifications -----"
+echo "##########################################################################"
+sleep 2 
+     echo "supression de Booty.sh des fichier executables"
+     echo "booty shake" sleep 1 echo "shake" sleep 1 echo "shaaaake"
+     chmod -x /booty.sh
+     mv  booty.sh .booty_shaked.sh  
+    
