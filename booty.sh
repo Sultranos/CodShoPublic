8f884391a4f73ea992eb60c9009aee5b999d4018
Sultranos <110790178+Sultranos@users.noreply.github.com>
2022-08-12 08:12:44 +0200

    Add files via upload

--- booty.sh
+++ booty.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
+echo "########################################################################"
+echo "Initialisation ----------"
+echo 0.5
+echo "Initialisation ***-------"
+echo 0.5
+echo "Initialisation ******----"
+echo 0.5
+echo "Initialisation *********-"
+echo 0.5
+echo "Initialisation Complete"
+ echo ""
     echo "Bonjour Nouvel Utilisateur,"
-    echo "a qui ai-je l'honneur de parler?"
+    echo "A qui ai-je l'honneur de parler?"
         read varname
         echo "Bienvenue sur le systeme $varname"
-        echo "est-ce le meme Utilisateur que $user? (y/n)"
+        echo "Est-ce le meme Utilisateur que $USER? (y/n)"
         read answers2
          if [[ $answer == "y" ]]; then 
-            $varname=$user
+            $varname=$USER
         fi
         echo "Desirez vous ajouter un utilisateur pi avec UID1000? (y/n)"
             read pi
             if [[ $pi == "y" ]]; then
                 echo "L'utilisateur pi seras ajouté lors de l'installation"
             elif [[ $answer == "y" ]]; then
-                    echo "tres bien, $user"
+                    echo "tres bien, $USER"
                 else
                     echo "Vous serez le seul utilisateur ajouter lors de l'installation"
             fi
@@ -22,8 +33,8 @@
 echo ""
 echo ""
 echo "###################################################################"
-echo "                 ------- Preparation pour Nala ------"
-echo "###################################################################"
+echo "                 ------- Installation de Nala ------"
+echo "##################################################################"
 echo ""
 sleep 2
     echo "Instalation des repertoires"    
@@ -32,25 +43,11 @@ sleep 2
 echo ""
     echo "Source Repo"
     echo "deb-src https://deb.volian.org/volian/ scar main" | sudo tee -a /etc/apt/sources.list.d/volian-archive-scar-unstable.list
-echo ""
-sleep 2
-echo "###################################################################"
-echo "                ------- Installation de Nala ------"
-echo "###################################################################"
-echo ""
 sleep 2
     sudo apt update
     echo "updated, installing Nala"
     sudo apt install nala -y || sudo apt install nala-legacy -y 
     echo "Nala is Installed"
-echo ""
-sleep 2
-echo "###################################################################"
-echo "                 ------- Mise a jour Nala ------"
-echo "###################################################################"
-echo ""
-sleep 2
-echo ""
     sudo nala update && sudo nala upgrade -y
 echo ""
 sleep 2
@@ -69,16 +66,17 @@ echo "###################################################################"
 echo ""
     sudo nala install mosquitto mosquitto-clients -y
        echo "Creer MDP pour Mosquitto"
-       read varpasswd
+       read -s varpasswd
        echo "$varname:$varpasswd" > pwfile
        mosquitto_passwd -U pwfile
        cat pwfile
        sudo mv pwfile /etc/mosquitto/
-       echo "copier et coller les lignes suivante dans le dos au dessu de "include_dir" "
+       echo "copier et coller les lignes suivante dans le dos au dessus de "include_dir" "
        sleep 1
        echo ""
             echo "allow_anonymous false"
             echo "password_file /etc/mosquitto/pwfile"
+            echo ""
             echo "contiue ? (Y/n)"
                 read answer2
                 if [[ $answer2 == "n" ]]; then
@@ -97,14 +95,15 @@ echo ""
     wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
     lsb_release -a
     echo "deb https://repos.influxdata.com/debian buster stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-    sudo nala update
+    sudo nala update 
     sudo nala install influxdb -y
     sudo systemctl unmask influxdb
     sudo systemctl enable influxdb
     sudo systemctl start influxdb
     influx
-    CREATE USER admin WITH PASSWORD 'adminpassword' WITH ALL PRIVILEGES
-    exit
+    xterm -hold -e "CREATE USER admin WITH PASSWORD 'adminpassword' WITH ALL PRIVILEGES" &
+    xterm -hold -e "exit" &
+echo ""
     echo "faire CTRL+W et chercher HTTP, faire les réglages suivants: "
         sleep 1
         echo ""
@@ -117,18 +116,22 @@ echo ""
                 if [[ $answer5 == "n" ]]; then
                     sleep 5 echo "contiue ? (Y/n)"
                     read answer6
-                if [[ $answer6 == "n" ]]; then
-                    sleep 5 echo "contiue ? (Y/n)"
-                    read answer7
-                if [[ $answer7 == "n" ]]; then
-                    sleep 5 
+                         if [[ $answer6 == "n" ]]; then
+                         sleep 5 echo "contiue ? (Y/n)"
+                         read answer7
+                             if [[ $answer7 == "n" ]]; then
+                             sleep 5 
+                             fi
+                         fi
                 fi 
+echo ""
     sudo nano /etc/influxdb/influxdb.conf
     sudo systemctl restart influxdb
-    influx -u $varname -p <$varpasswd>
-    CREATE DATABASE sensors
-    exit
-    sudo nala install build-essential git
+    influx -user $varname -password <$varpasswd>' 
+    #'
+    xterm -hold -e "CREATE DATABASE Sensors" &
+    xterm -hold -e "exit" &
+    sudo nala install build-essential git -y
     bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
     npm install node-red-contrib-influxdb
     sudo systemctl enable nodered.service
@@ -139,14 +142,15 @@ echo ""
     sudo nala install grafana -y
     sudo systemctl enable grafana-server
     sudo systemctl start grafana-server    
-    while [ mosquitto_sub -u mqtt_username -P mqtt_password -v -t "#" ]; do 
+    while [ sleep 8 ]; do
+        mosquitto_sub -user mqtt_username -P mqtt_password -v -t "#"  
         sleep5 
         exit
     done
 echo ""
 sleep 2
 echo "###################################################################"
-echo "                 -------System mis a jour------"
+echo "                 ------- System mis a jour ------"
 echo "###################################################################"
 echo ""
 sleep 1
@@ -166,11 +170,11 @@ echo "                   ----- Création des Utilisateurs -----"
 echo "############################################################################"
 sleep 2 
     if [[ $pi == "y" ]]; then
-            usermod -u 1001 $user
-            groupmod -g 1001 $user
-            find / -group 1000 -printO | xargs -O chgrp -h $user
-            find / -user 1000 printO | xargs -O chown -h $user
-            usermod -aG 1001 $user
+            usermod -u 1001 $USER
+            groupmod -g 1001 $USER
+            find / -group 1000 -printO | xargs -O chgrp -h $USER
+            find / -user 1000 printO | xargs -O chown -h $USER
+            usermod -aG 1001 $USER
         useradd -u 1000 -m -k -N -s /bin/bash -G sudo pi || sudo usermod -aG sudo pi
         addgroup --force-badname Many-Faces-God
         usermod -aG Many-Faces-God pi 
@@ -179,14 +183,14 @@ sleep 2
     else 
         addgroup --force-badname Many-Faces-God
     fi
-    if [[ $user == $varname ]]; then
+    if [[ $USER == $varname ]]; then
         usermod -aG sudo $varname && usermod -aG Many-Faces-God $varname
         echo "Utilisateur $varname a été ajouté au groupes d'administration"
     else
-    adduser --force-badname $varname
-    usermod -aG sudo $varname && usermod -aG Many-Faces-God $varname
-        echo "Utilisateur $varname créé"
-        echo "Utilisateur $varname a été ajouté au groupes d'administration"
+        adduser --force-badname $varname
+        usermod -aG sudo $varname && usermod -aG Many-Faces-God $varname
+            echo "Utilisateur $varname créé"
+            echo "Utilisateur $varname a été ajouté au groupes d'administration"
     fi
 echo ""
 echo "##########################################################################"
@@ -194,16 +198,18 @@ echo ""
     gnome-terminal htop &
     neofetch
     sleep 2
-    echo "$varname, le systeme est pret à etre utilisé"
+    echo "$varname, le systeme est prêt à etre utilisé"
     echo "Enjoy =) "
     sudo su - $varname
 echo "##########################################################################"
-echo "                   ----- Dernieres Modifications -----"
+echo "                   ----- Dernières Modifications -----"
 echo "##########################################################################"
 sleep 2 
-     echo "supression de Booty.sh des fichier executables"
+     echo "Suppression de Booty.sh des fichier executables"
      echo "booty shake" 
-     sleep 1 echo "shake" 
-     sleep 1 echo "shaaaake"
-     chmod -x /booty.sh
+     sleep 1 
+     echo "shake" 
+     sleep 1 
+     echo "shaaaake"
+     chmod -x booty.sh
      mv  booty.sh .booty_shaked.sh  
